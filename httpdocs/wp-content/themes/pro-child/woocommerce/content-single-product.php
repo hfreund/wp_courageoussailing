d1f51888726615e0e565e5bd98067ec2fad4cd2e
Hugh Freund <hugh.freund@gmail.com>
2017-11-20 22:26:40 -0500

    cleaned up file

--- httpdocs/wp-content/themes/pro-child/woocommerce/content-single-product.php
+++ httpdocs/wp-content/themes/pro-child/woocommerce/content-single-product.php
@@ -38,19 +38,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	
-
 	<div class="summary entry-summary">
+
 		<?php
 			/**
-		 	* woocommerce_before_single_product_summary hook.
-			 *
-		 	* @hooked woocommerce_show_product_sale_flash - 10
-		 	* @hooked woocommerce_show_product_images - 20
-		 	*/
+			* woocommerce_before_single_product_summary hook.
+			*
+			* @hooked woocommerce_show_product_sale_flash - 10
+			* @hooked woocommerce_show_product_images - 20
+			*/
 			do_action( 'woocommerce_before_single_product_summary' );
-		?>
-		<?php
 			/**
 			 * woocommerce_single_product_summary hook.
 			 *
@@ -66,7 +63,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		?>
 
 	</div><!-- .summary -->
-
+	 <div class="product-description">
 	<?php
 		/**
 		 * woocommerce_after_single_product_summary hook.
@@ -79,7 +76,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	?>
 
 	<meta itemprop="url" content="<?php the_permalink(); ?>" />
-
+	 </div>
 </div><!-- #product-<?php the_ID(); ?> -->
 
 <?php do_action( 'woocommerce_after_single_product' ); ?>
